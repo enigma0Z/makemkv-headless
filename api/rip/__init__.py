63a5a402d5257104c719cadec83994231a36787f
enigma0Z <enigma.0ZA@gmail.com>
2025-05-30 22:14:28 -0400

    Sanitize output for makemkvcon.txt

--- api/rip/__init__.py
+++ api/rip/__init__.py
@@ -5,6 +5,8 @@ import tempfile
 import threading
 
 import logging
+
+from util import sanitize
 logger = logging.getLogger(__name__)
 
 from sort import SortInfo, sort_titles
@@ -58,7 +60,7 @@ def rip_titles(
     interface.print(f"These titles will be copied to {sort_info.base_path()}", target=Target.SORT)
     logger.info(f"These titles will be copied to {sort_info.base_path()}")
 
-    with open(os.path.join(rip_path, f'{toc.source.name}-makemkvcon.txt'), 'w') as file:
+    with open(os.path.join(rip_path, sanitize(f'{toc.source.name}-makemkvcon.txt')), 'w') as file:
       file.writelines(toc.lines)
       file.write(toc.source)
 
