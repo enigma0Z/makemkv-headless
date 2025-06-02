dfba061890e4c940e79e3185fa8404e062ad2aab
enigma0Z <enigma.0ZA@gmail.com>
2025-06-01 21:59:28 -0400

    Add config reload endpoint

--- app/api/v1/config.py
+++ app/api/v1/config.py
@@ -6,3 +6,9 @@ from config import CONFIG
 @json_serializable_api
 def get_config():
   return CONFIG
+
+@API.get('/api/v1/config/reload')
+@json_serializable_api
+def get_config_reload():
+  CONFIG.update_from_file()
+  return CONFIG
