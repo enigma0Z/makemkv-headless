636db3b8b36c690efaf857e34b8cb31585f202bb
enigma0Z <enigma.0ZA@gmail.com>
2025-06-03 09:42:33 -0400

    Rearrange default files and intiial set up

---
+++ web/src/pages/App.tsx
@@ -0,0 +1,15 @@
+import { TOCTable } from '@/components/toc/TOCTable'
+import { CombinedShowMovieForm } from '@/components/forms/CombinedShowMovieForm'
+
+function App() {
+  return (
+    <>
+      <div>
+      </div>
+      <CombinedShowMovieForm />
+      <TOCTable />
+    </>
+  )
+}
+
+export default App
