6b9867279b508f4e0a079947ae8980ba17600b8f
enigma0Z <enigma.0ZA@gmail.com>
2025-06-03 09:44:28 -0400

    Add supporting types for redux, api

---
+++ web/src/api/types/SortInfo.ts
@@ -0,0 +1,13 @@
+export type SortInfo = {
+  name?: string;
+  id?: string;
+  main_indexes: number[];
+  extra_indexes: number[];
+  split_segments?: number[];
+  id_db?: string;
+}
+
+export type ShowInfo = SortInfo & {
+  season_number: number;
+  first_episode: number;
+}
---
+++ web/src/api/types/Toc.ts
@@ -0,0 +1,52 @@
+export type Toc = {
+  lines: string[];
+  source: BaseInfo & SourceInfo;
+}
+
+export type BaseInfo = {
+  fields: string[];
+}
+
+export type SourceInfo = {
+  titles: BaseInfo & TitleInfo[];
+  name: string;
+  name1: string;
+  name2: string;
+  name3: string;
+  media: string;
+}
+
+export type TitleInfo = {
+  tracks: BaseInfo & (
+    BaseTrackInfo 
+    | BaseTrackInfo & VideoTrackInfo 
+    | BaseTrackInfo & AudioTrackInfo
+  )[];
+  chapters: string;
+  runtime: string;
+  size: string;
+  segments: string;
+  segments_map: string;
+  filename: string;
+  summary: string;
+}
+
+export type BaseTrackInfo = {
+  stream_type?: string;
+  stream_format?: string;
+  stream_conversion_type?: string;
+  stream_bitrate?: string;
+  stream_detail?: string;
+  stream_language_code?: string;
+  stream_language?: string;
+}
+
+export type VideoTrackInfo = {
+  video_resolution?: string;
+  video_aspect_ratio?: string;
+  video_framerate?: string;
+}
+
+export type AudioTrackInfo = {
+  audio_format?: string
+}
