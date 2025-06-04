19c2ac892ceb281ad001be2f83627f34bd5dab9d
enigma0Z <enigma.0ZA@gmail.com>
2025-06-03 23:17:51 -0400

    Add processing for best-guess of show/movie main and extra titles

--- web/src/components/toc/TOCTable.tsx
+++ web/src/components/toc/TOCTable.tsx
@@ -1,6 +1,7 @@
 import { useAppDispatch, useAppSelector } from "@/api/store"
 import { ripActions } from "@/api/store/rip"
 import type { TitleInfo, Toc } from "@/api/types/Toc"
+import { hmsToSeconds } from "@/util/string"
 import { Button, Card, Checkbox, FormControlLabel, Radio, RadioGroup, Table, TableCell, TableContainer, TableHead, TableRow } from "@mui/material"
 import { useEffect, useState } from "react"
 
@@ -8,15 +9,112 @@ type TableProps = {
   content?: "show" | "movie"
 }
 
-export const TOCTable = ( { content }: TableProps) => {
+type TitleGroup = {
+  title: TitleInfo,
+  index: number,
+  matches: number[]
+}
+
+const EPISODE_LENGTH_TOLERANCE_SECONDS = 120
+
+export const TOCTable = () => {
   const dispatch = useAppDispatch()
 
   const mainIndexes = useAppSelector((state) => state.rip.sort_info.main_indexes)
   const extraIndexes = useAppSelector((state) => state.rip.sort_info.extra_indexes)
+  const content = useAppSelector((state) => state.rip.destination.content)
 
   const [tocData, setTocData] = useState<Toc>()
+  const [tocLoading, setTocLoading] = useState<boolean>(false)
   const [oldMainIndexes, setOldMainIndexes] = useState<number[]>([])
   const [oldExtraIndexes, setOldExtraIndexes] = useState<number[]>([])
+
+  const getLongestTitle = () => {
+    let longestTitleIndex = 0
+    let longestTitleLength = 0
+    tocData?.source.titles.forEach((title, index) => {
+      const outerTitleLength = hmsToSeconds(title.runtime)
+      if (outerTitleLength > longestTitleLength) {
+        longestTitleLength = outerTitleLength
+        longestTitleIndex = index
+      }
+    })
+
+    return {
+      longestTitleLength,
+      longestTitleIndex
+    }
+  }
+
+  const getLongestTitleGroup = () => {
+    /**
+     * [
+     *   title: TitleInfo
+     *   index: number
+     *   matches: TitleInfo[]
+     * ]
+     */
+    const titleGroups: TitleGroup[] = []
+
+    const matchedIndexes = () => (titleGroups.map((titleGroup) => (
+      titleGroup.matches
+    )).flat())
+
+    tocData?.source.titles.forEach((outerTitle, outerIndex) => {
+      const newTitleGroup: TitleGroup = { title: outerTitle, index: outerIndex, matches: [] }
+      const outerTitleLength = hmsToSeconds(outerTitle.runtime)
+      tocData.source.titles.forEach((innerTitle, innerIndex) => {
+        if (
+          !(innerIndex in matchedIndexes())
+          && hmsToSeconds(innerTitle.runtime) > outerTitleLength - EPISODE_LENGTH_TOLERANCE_SECONDS
+          && hmsToSeconds(innerTitle.runtime) < outerTitleLength + EPISODE_LENGTH_TOLERANCE_SECONDS
+          
+        ) {
+          newTitleGroup.matches.push(innerIndex)
+        }
+      })
+      titleGroups.push(newTitleGroup)
+    })
+
+    return {
+      titleGroups: titleGroups,
+      longestTitleGroup: titleGroups.reduce((previous, current) => {
+        if (previous.matches.length < current.matches.length) {
+          return current
+        } else {
+          return previous
+        }
+      })
+    }
+  }
+
+  const getMovieIndexes = () => {
+    const { longestTitleIndex } = getLongestTitle()
+    const mainIndexes = []
+    const extraIndexes = []
+    for (let i=0; i < (tocData?.source.titles.length ?? 0); i++) {
+      if (i == longestTitleIndex) {
+        mainIndexes.push(i)
+      } else {
+        extraIndexes.push(i)
+      }
+    }
+    return {mainIndexes, extraIndexes}
+  }
+  
+  const getShowIndexes = () => {
+    const {longestTitleGroup} = getLongestTitleGroup()
+    const mainIndexes = []
+    const extraIndexes = []
+    for (let i=0; i < (tocData?.source.titles.length ?? 0); i++) {
+      if (longestTitleGroup.matches.indexOf(i) > -1) {
+        mainIndexes.push(i)
+      } else {
+        extraIndexes.push(i)
+      }
+    }
+    return {mainIndexes, extraIndexes}
+  }
 
   const handleLoadTocClick = () => {
     console.info('Fetching TOC')
@@ -29,7 +127,30 @@ export const TOCTable = ( { content }: TableProps) => {
   }
 
   useEffect(() => {
-    dispatch(ripActions.setTocLength(tocData?.source.titles.length))
+    if (tocData) {
+      dispatch(ripActions.setTocLength(tocData.source.titles.length))
+
+      let longestTitleIndex = 0
+      let longestTitleLength = 0
+      tocData.source.titles.forEach((title, index) => {
+        const outerTitleLength = hmsToSeconds(title.runtime)
+        if (outerTitleLength > longestTitleLength) {
+          longestTitleLength = outerTitleLength
+          longestTitleIndex = index
+        }
+      })
+
+      console.debug('longestTitleIndex', longestTitleIndex)
+      console.debug('longestTitleLength', longestTitleLength)
+
+      let getIndexes: () => { mainIndexes: number[], extraIndexes: number[] }
+      if (content === "movie") getIndexes = getMovieIndexes
+      else getIndexes = getShowIndexes
+
+      const {mainIndexes, extraIndexes} = getIndexes()
+      dispatch(ripActions.setMainIndexes(mainIndexes))
+      dispatch(ripActions.setExtraIndexes(extraIndexes))
+    }
   }, [tocData]) 
 
   const handleSelectAllOnClick = (event: React.ChangeEvent, checked: boolean) => {
