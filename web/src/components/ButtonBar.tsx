a662e0a7b7f64cc8c6bf5689a4183f8defca123d
enigma0Z <enigma.0ZA@gmail.com>
2025-07-19 23:33:17 -0400

    Move button bar actions to button bar component, implement appbar

--- web/src/components/ButtonBar.tsx
+++ web/src/components/ButtonBar.tsx
@@ -1,34 +1,120 @@
-import { Button } from "@mui/material";
-import { useContext } from "react";
-import { Context } from "./socket/Context";
+import { AppBar, Button, IconButton, Toolbar, Tooltip } from "@mui/material";
+import { useAppDispatch, useAppSelector } from "@/api/store";
+import { socketActions } from "@/api/store/socket";
+import endpoints from "@/api/endpoints";
+import { tocActions } from "@/api/store/toc";
+import { ConfirmationDialog } from "./ConfirmationModal";
+import { useState } from "react";
 
-type Props = {
-  onEject: () => void;
-  onLoadToc: () => void;
-  onStartRip: () => void;
-  ripDisabled: boolean;
-}
+import MenuIcon from '@mui/icons-material/Menu';
+import EjectIcon from '@mui/icons-material/Eject';
+import SaveAltIcon from '@mui/icons-material/SaveAlt';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import CancelIcon from '@mui/icons-material/Cancel';
 
-export const ButtonBar = ({ onEject, onLoadToc, onStartRip, ripDisabled }: Props) => {
-  const { ripState } = useContext(Context)
+type Props = {}
 
-  return <div>
-    <Button
-      onClick={ onEject }
-      disabled={ ripState?.rip_started }
-    >
-      Eject
-    </Button>
-    <Button
-      onClick={ onLoadToc }
-      disabled={ ripState?.rip_started }
-    >
-      Load TOC
-    </Button>
-    <Button
-      onClick={ onStartRip }
-    >
-      { ripState?.rip_started ? "Cancel Rip" : "Start Rip" }
-    </Button>
-  </div>
+export const ButtonBar = ({ }: Props) => {
+  const dispatch = useAppDispatch()
+
+  const sortInfo = useAppSelector((state) => state.rip.sort_info)
+  const library = useAppSelector((state) => state.rip.destination?.library)
+  const media = useAppSelector((state) => state.rip.destination?.media)
+  const content = useAppSelector((state) => state.rip.destination?.content)
+  const ripAll = useAppSelector((state) => state.rip.rip_all)
+
+  const ripState = useAppSelector((state) => state.socket.ripState)
+
+  const [cancelModalOpen, setCancelModalOpen] = useState<boolean>(false)
+
+  const handleEject = () => {
+    console.info('Ejecting disc')
+    dispatch(socketActions.resetSocketState({}))
+    fetch(endpoints.eject(), { method: 'GET' })
+  }
+
+  const handleLoadToc = () => {
+    console.info('Fetching TOC')
+    dispatch(tocActions.setTocData(undefined))
+    // fetch(endpoints.state.resetSocket(), { method: 'GET' })
+    fetch(endpoints.toc(), { method: 'GET' })
+      .then(response => response.json())
+      .then(json => {
+        dispatch(tocActions.setTocData(json))
+      })
+  }
+
+  const handleCancelRip = () => {
+    fetch(endpoints.rip.stop(), {
+      method: 'GET'
+    }).then((response) => {
+      console.log('Rip stop response', response)
+    })
+  };
+
+  const handleStartRip = () => {
+    if (ripState?.rip_started) {
+      setCancelModalOpen(true)
+    } else {
+      dispatch(socketActions.resetSocketState({ rip_started: true }))
+      fetch(endpoints.rip.start(), {
+        method: 'POST',
+        body: JSON.stringify({
+          rip_all: ripAll,
+          destination: `${library}/${content}s/${media}`,
+          sort_info: sortInfo
+        }),
+        headers: new Headers({
+          "Content-Type": "application/json"
+        })
+      })
+    }
+  }
+
+  return <>
+    <AppBar>
+      <Toolbar>
+        <Tooltip title="Menu">
+          <IconButton>
+            <MenuIcon />
+          </IconButton>
+        </Tooltip>
+        <Tooltip title="Eject Disc">
+          <IconButton
+            onClick={handleEject}
+            disabled={ripState?.rip_started}
+          >
+            <EjectIcon />
+          </IconButton>
+        </Tooltip>
+        <Tooltip title="Load ToC (Table of Contents)">
+          <IconButton
+            onClick={handleLoadToc}
+            disabled={ripState?.rip_started}
+          >
+            <SaveAltIcon />
+          </IconButton>
+        </Tooltip>
+        <Button
+          sx={{ marginLeft: "auto" }}
+          onClick={handleStartRip}
+          variant="outlined"
+          startIcon={ ripState?.rip_started ? <CancelIcon /> : <CheckCircleIcon /> }
+        >
+          { ripState?.rip_started 
+            ? "Cancel Rip"
+            : "Start Rip"
+          }
+        </Button>
+        <ConfirmationDialog
+          open={cancelModalOpen}
+          onClose={() => { setCancelModalOpen(false) }}
+          onConfirm={() => { handleCancelRip() }}
+          title={'Cancel Rip?'}
+          message={'This disc will not get uploaded if it is cancelled now'}
+        />
+      </Toolbar>
+    </AppBar>
+    <div style={{ height: "4rem" }} />
+  </>
 }
--- web/src/pages/App.tsx
+++ web/src/pages/App.tsx
@@ -1,30 +1,19 @@
-import { TOCTable } from '@/components/toc/TOCTable'
 import { CombinedShowMovieForm } from '@/components/forms/ContentForm'
-import { StatusScroller } from '@/components/status/StatusScroller'
 import { AppContainer } from './App.styles'
 import { ButtonBar } from '@/components/ButtonBar'
-import { useContext, useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import endpoints from '@/api/endpoints'
 import { useAppDispatch, useAppSelector } from '@/api/store'
-import { Context } from '@/components/socket/Context'
 import { tocActions } from '@/api/store/toc'
 import { tmdbActions } from '@/api/store/tmdb'
 import type { ApiState } from '@/api/types/status'
 import { ripActions } from '@/api/store/rip'
-import { ConfirmationDialog } from '@/components/ConfirmationModal'
+import { StatusScroller } from '@/components/status/StatusScroller'
+import { TOCGrid } from '@/components/toc/TOCGrid'
 
 function App() {
   const dispatch = useAppDispatch()
-  const { ripState, setRipState, resetRipState } = useContext(Context)
 
-  const [error, setError] = useState<boolean>()
-  const [cancelModalOpen, setCancelModalOpen] = useState<boolean>(false)
-
-  const sortInfo = useAppSelector((state) => state.rip.sort_info)
-  const library = useAppSelector((state) => state.rip.destination?.library)
-  const media = useAppSelector((state) => state.rip.destination?.media)
-  const content = useAppSelector((state) => state.rip.destination?.content)
-  const ripAll = useAppSelector((state) => state.rip.rip_all)
   const tmdbConfiguration = useAppSelector((state) => state.tmdb.configuration)
 
   if (!tmdbConfiguration) {
@@ -33,54 +22,6 @@ function App() {
       .then(json => {
         dispatch(tmdbActions.setTmdbConfiguration(json))
       })
-  }
-
-  const handleEject= () => {
-    console.info('Ejecting disc')
-    resetRipState && resetRipState()
-    fetch(endpoints.eject(), { method: 'GET' })
-  }
-
-  const handleLoadTocClick = () => {
-    console.info('Fetching TOC')
-    resetRipState && resetRipState()
-    dispatch(tocActions.setTocData(undefined))
-    // fetch(endpoints.state.resetSocket(), { method: 'GET' })
-    fetch(endpoints.toc(), { method: 'GET' })
-      .then(response => response.json())
-      .then(json => {
-        dispatch(tocActions.setTocData(json))
-      })
-  }
-
-  const handleCancelRip = () => {
-    fetch(endpoints.rip.stop(), { 
-      method: 'GET'
-    }).then((response) => {
-      setRipState && setRipState({ rip_started: false })
-      console.log('Rip stop response', response)
-    })
-  };
-
-  const handleStartRip = () => {
-    if (ripState?.rip_started) {
-      setCancelModalOpen(true)
-    } else {
-      setRipState && setRipState({ rip_started: true })
-      fetch(endpoints.rip.start(), { 
-        method: 'POST',
-        body: JSON.stringify({
-          rip_all: ripAll,
-          destination: `${library}/${content}s/${media}`,
-          sort_info: sortInfo
-        }),
-        headers: new Headers({
-          "Content-Type": "application/json"
-        })
-      }).then((response) => {
-        resetRipState && resetRipState();
-      })
-    }
   }
 
   useEffect(() => {
@@ -103,26 +44,10 @@ function App() {
 
   return (
     <AppContainer>
-      {/* TODO: Add drive control buttons */}
-      <ButtonBar 
-        onEject={handleEject}
-        onLoadToc={handleLoadTocClick}
-        onStartRip={handleStartRip}
-        ripDisabled={error ?? true}
-      />
-      <CombinedShowMovieForm 
-        onError={() => setError(true)}
-        onClearError={() => setError(false)}
-      />
-      <TOCTable />
+      <ButtonBar />
+      <CombinedShowMovieForm />
+      <TOCGrid />
       <StatusScroller /> 
-      <ConfirmationDialog
-        open={cancelModalOpen}
-        onClose={() => { setCancelModalOpen(false) } }
-        onConfirm={() => { handleCancelRip() } } 
-        title={'Cancel Rip?'} 
-        message={'This disc will not get uploaded if it is cancelled now'}
-      />
     </AppContainer>
   )
 }
