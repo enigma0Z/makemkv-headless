1ae8dd781c06c34ca1909f711209dba9101b3273
enigma0Z <enigma.0ZA@gmail.com>
2025-10-04 15:41:57 -0400

    Async TOC endpoint

--- api/src/api/v1/toc.py
+++ api/src/api/v1/toc.py
@@ -3,7 +3,7 @@ import logging
 from threading import Lock
 from traceback import format_exc
 
-from fastapi import APIRouter
+from fastapi import APIRouter, BackgroundTasks
 
 from src.api.api_response import GenericAPIError, APIException, APIResponse
 from src.api.state import STATE
@@ -16,34 +16,56 @@ class TOCError(Exception): ...
 logger = logging.getLogger(__name__)
 
 lock = Lock()
+toc = TOC()
 
 router = APIRouter(prefix="/toc")
 
 # @lru_cache
 async def get_toc_from_disc(source):
-  toc = TOC()
-  await toc.get_from_disc(source)
-  return toc
+  with lock:
+    await toc.get_from_disc(source)
+    STATE.redux.toc = toc
+    return toc
 
 @router.get('')
 @router.get('/')
 async def get_toc():
   try:
-    with lock:
-      toc = await get_toc_from_disc(CONFIG.source)
-      failures = toc.get_failures()
+    toc = await get_toc_from_disc(CONFIG.source)
+    failures = toc.get_failures()
 
-      if len(failures) > 0: 
-        raise TOCError(failures)
+    if len(failures) > 0: 
+      raise TOCError(failures)
 
-      STATE.redux.toc = toc
-      return APIResponse("success", toc)
+    STATE.redux.toc = toc
+    return APIResponse("success", toc)
   except TOCError as ex:
     logger.error(ex.args[0])
     raise APIException(404, failures)
   except Exception as ex:
     logger.error(format_exc())
     raise APIException(500, GenericAPIError("error", ex))
+
+@router.get('.async')
+async def get_toc_async(background_tasks: BackgroundTasks):
+  try:
+    if lock.locked():
+      return APIResponse("in progress")
+    else:
+      background_tasks.add_task(get_toc_from_disc, CONFIG.source)
+      return APIResponse("started", toc)
+  except TOCError as ex:
+    logger.error(ex.args[0])
+  except Exception as ex:
+    logger.error(format_exc())
+    raise APIException(500, GenericAPIError("error", ex))
+
+@router.get('.async.status')
+async def get_toc_async(background_tasks: BackgroundTasks):
+  if lock.locked():
+    return APIResponse("in progress")
+  else:
+    return APIResponse("done", toc)
 
 @router.get('.cache.clear')
 async def toc_clear_cache():
