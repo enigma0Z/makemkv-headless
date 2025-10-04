0798def4fcb1f6c9647dd45eb1fb529193385f28
enigma0Z <enigma.0ZA@gmail.com>
2025-10-04 14:27:30 +0000

    screen based run

--- run.sh
+++ run.sh
@@ -1,41 +1,4 @@
 #!/bin/bash
 
-function run_api() {
-  cd api
-
-  if [ ! -d .venv ]; then
-    python3 -m venv .venv
-  fi
-
-  source .venv/bin/activate
-  pip install -r requirements.txt
-
-  python3 -m src $@ &
-  echo $!
-}
-
-function run_web() {
-  cd web
-  npm install
-  npm run dev $@ &
-  echo $!
-}
-
-./api.sh &
-API_PID=$!
-
-./web.sh &
-WEB_PID=$!
-
-function ctrl_c() {
-  kill -9 $WEB_PID
-  kill -9 $API_PID
-}
-
-echo "API: $API_PID"
-echo "WEB: $WEB_PID"
-
-trap ctrl_c INT
-
-wait $API_PID $WEB_PID
-kill -9 $API_PID $WEB_PID
+screen -dmS api ./api.sh
+screen -dmS web ./web.sh
