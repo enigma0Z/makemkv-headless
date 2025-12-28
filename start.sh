7dc73f1bab891b5befaeacb2d27d923e33f0e55a
enigma0Z <enigma.0ZA@gmail.com>
2025-12-28 13:09:33 -0500

    Adjust startup script params

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 api_port="$1"; shift
-web_ip="$1"; shift
 web_port="$1"; shift
 
 if [[ -z "$api_port" ]]; then
@@ -12,5 +11,5 @@ if [[ -z "$web_port" ]]; then
 	web_port=3000
 fi
 
-screen -dmS makemkv-headless-api-$api_port ./api.sh --port $api_port --frontend "http://${web_ip}:${web_port}" "@$"
+screen -dmS makemkv-headless-api-$api_port ./api.sh --port $api_port "@$"
 screen -dmS makemkv-headless-web-$web_port ./web.sh --port $web_port
