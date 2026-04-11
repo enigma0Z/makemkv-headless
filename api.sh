b17f8d45d862fce293238c68f55f28be6e6b3304
enigma0Z <enigma.0ZA@gmail.com>
2026-04-11 12:18:54 +0000

    Update api.sh to use UV

--- api.sh
+++ api.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 cd api
 
-# rm -rvf .venv
-python3.13 -m venv .venv
-
-source .venv/bin/activate
-pip install -r requirements.txt
-
-exec python3 -m src.main_api "$@"
+uv run mmh_api "$@"
