19e8098e7aee4b309d312cc43187523fe99ccc03
enigma0Z <enigma.0ZA@gmail.com>
2026-04-14 17:35:07 -0400

    Startup script for split ports

--- start.sh
+++ start.sh
@@ -1,15 +1,44 @@
 #!/bin/bash
 
 SCREEN="false"
-LISTEN_PORT=""
+DEV="false"
+API_PORT=""
+WEB_PORT=""
 
 if [[ "$1" == "screen" ]]; then
   SCREEN="true"; shift
-  LISTEN_PORT="$1"; shift
+  WEB_PORT="$1"; shift
+fi
+
+if [[ "$1" == "dev" ]]; then
+  DEV="true"; shift
+  API_PORT="$1"; shift
+  WEB_PORT="$1"; shift
 fi
 
 if [[ "$SCREEN" == "true" ]]; then
-  screen -dmS mmh-api-$LISTEN_PORT uv --project api run mmh_api --listen-port $LISTEN_PORT "$@"
+  screen -dmS mmh-api-$API_PORT uv --project api run mmh_api --listen-port $API_PORT "$@"
+elif [[ "$DEV" == "true" ]]; then
+  WEB_PID=""
+  API_PID=""
+  cleanup() {
+    echo "Cleaning up"
+    set -x
+    kill $WEB_PID
+    kill $API_PID
+    set +x
+  }
+
+  uv --project api run mmh_api --listen-port $API_PORT "$@" &
+  API_PID=$!
+
+  export VITE_BACKEND_PORT=$API_PORT
+  cd web
+  npm run dev -- --port 3000
+  WEB_PID=$!
+
+  trap 'cleanup' SIGINT
+  wait $API_PID $WEB_PID
 else
   uv --project api run mmh_api "$@"
 fi
