132cda1a7e2e507a229290e8fc951123167dcb8d
enigma0Z <enigma.0ZA@gmail.com>
2026-04-20 14:55:34 -0400

    Unified logging + daemon mode

--- api/src/makemkv_headless/cli/subcommands/start.py
+++ api/src/makemkv_headless/cli/subcommands/start.py
@@ -1,36 +1,59 @@
 
 from argparse import Namespace
+from importlib import resources
 import logging
+import logging.config
 from sys import argv
 import uvicorn
 
 from os import fork
+from os.path import basename
+
+import yaml
 
 from makemkv_headless.config import CONFIG
 from makemkv_headless.cli.parsers import subparsers
 
+from makemkv_headless import config
+
 def main(opts: Namespace):
   CONFIG.load(opts)
+
+  with open(resources.files(config) / 'logging.yaml') as file:
+    log_config = yaml.safe_load(file)
+    log_config['loggers']['']['level'] = CONFIG.log_level
+    if CONFIG.log_file:
+      log_config['handlers']['default']['class'] = 'logging.FileHandler'
+      log_config['handlers']['default']['filename'] = CONFIG.log_file
 
   # Erase log file
   open(CONFIG.log_file, 'w').close()
 
-  logging.basicConfig(
-    style='{', 
-    format='{asctime} [{levelname}] {filename}:{lineno} {threadName} - {message}', 
-    level=CONFIG.get_log_level(),
-    filename=CONFIG.log_file,
-    filemode='a'
-  )
+  logging.config.dictConfig(log_config)
 
   logger = logging.getLogger(__name__)
 
   CONFIG.normalize_paths()
   from makemkv_headless.api import app
   logger.info(f'Starting app {app} with config {CONFIG}')
-  config = uvicorn.Config(app=app, host="0.0.0.0", port=CONFIG.listen_port)
-  server = uvicorn.Server(config)
-  server.run()
+  server_config = uvicorn.Config(
+    app=app, 
+    host="0.0.0.0", 
+    port=CONFIG.listen_port,
+    log_config=log_config
+  )
+
+  pid = 0
+
+  if (opts.daemon):
+    pid = fork()
+
+  if (pid == 0):
+    server = uvicorn.Server(server_config)
+    server.run()
+  else:
+    with open(opts.pid_file, 'w') as file:
+      print(pid, file=file)
 
 parser = subparsers.add_parser('start', help='Start the makemkv headless server')
 parser.set_defaults(func=main)
@@ -45,5 +68,5 @@ parser.add_argument(
 parser.add_argument(
   '--pid-file', 
   help='The PID file to store the daemon pid in',
-  default=f'{argv[0]}.pid'
+  default=f'{basename(argv[0])}.pid'
 )
