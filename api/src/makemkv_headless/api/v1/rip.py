0d6e7b8499637ee79a26299a0cae1a60a08868ba
enigma0Z <6117628+enigma0Z@users.noreply.github.com>
2026-06-03 00:11:12 -0400

    Utilize cancellable async

--- api/src/makemkv_headless/api/v1/rip.py
+++ api/src/makemkv_headless/api/v1/rip.py
@@ -1,8 +1,9 @@
-from asyncio import Lock, Task, create_task
+from asyncio import Event, Lock, Task
 import logging
 import os
 
 from fastapi import APIRouter, BackgroundTasks
+from makemkv_headless.util import cancellable_async
 from pydantic import BaseModel
 
 from makemkv_headless.api.api_response import GenericAPIError, APIException, APIResponse
@@ -30,21 +31,30 @@ rip_task: Task | None = None
 rip_request: RequestModel | None = None
 
 LOCK = Lock()
+CANCEL = Event()
 
 async def rip_task_fn(data: RequestModel):
+  assert CONFIG.source is not None
+  assert CONFIG.destination is not None
+  assert CONFIG.temp_prefix is not None
+
   async with LOCK:
     logger.debug(f'rip_task_fn(), {rip_task}')
     STATE.reset_socket()
     STATE.socket.rip.started = True
     toc = Toc()
-    await toc.get_from_disc(CONFIG.source)
-    await rip_titles(
-      source=CONFIG.source,
-      dest_path=os.path.join(CONFIG.destination, data.destination),
-      sort_info=data.sort_info,
-      toc=toc,
-      rip_all=data.rip_all,
-      temp_prefix=CONFIG.temp_prefix
+
+    await cancellable_async(toc.get_from_disc(CONFIG.source), CANCEL)
+    await cancellable_async(
+      rip_titles(
+        source=CONFIG.source,
+        dest_path=os.path.join(CONFIG.destination, data.destination),
+        sort_info=data.sort_info,
+        toc=toc,
+        rip_all=data.rip_all,
+        temp_prefix=CONFIG.temp_prefix
+      ),
+      CANCEL
     )
 
     get_interface().send(RipStartStopMessage(state="stop"))
@@ -74,15 +84,16 @@ async def get_rip():
     
 @router.get('.stop')
 async def get_rip_stop():
-  global rip_task
   try:
-    if rip_task != None:
-      if not rip_task.done():
-        rip_task.cancel()
-        interface.send(RipStartStopMessage(state="stop"))
-        return APIResponse(status="in progress")
+    if LOCK.locked():
+      CANCEL.set()
+      interface.send(RipStartStopMessage(state="stop"))
+      return APIResponse(status="in progress")
 
     return APIResponse(status="stopped")
     
+  except IndexError as ex:
+    raise APIException(500, GenericAPIError("error", Exception("No rip task found")))
+
   except Exception as ex:
     raise APIException(500, GenericAPIError("error", ex))
