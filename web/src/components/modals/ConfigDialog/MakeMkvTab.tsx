df0841701d7d9e4fdc87433e65201d9ab316cfc0
enigma0Z <6117628+enigma0Z@users.noreply.github.com>
2026-06-11 17:22:42 -0400

    [minor] Config dialog rewrite

---
+++ web/src/components/modals/ConfigDialog/MakeMkvTab.tsx
@@ -0,0 +1,20 @@
+import { ConfigTextField } from "@/components/fields/config/ConfigTextField"
+import { Link, List } from "@mui/material"
+
+export const MakeMkvTab = () => {
+
+  return <>
+    <ConfigTextField
+      configItem="makemkvcon_path"
+      label="Path to makemkvcon"
+    />
+    <ul>
+      <li>
+        <Link href="https://www.makemkv.com">Get MakeMKV</Link> (<Link href="https://forum.makemkv.com/forum/viewtopic.php?f=3&t=224">for Linux</Link>)
+      </li>
+      <li>
+        <Link href="https://www.makemkv.com/buy/">Purchase a license for MakeMKV</Link>
+      </li>
+    </ul>
+  </>
+}
--- web/src/components/modals/ConfigDialog/index.tsx
+++ web/src/components/modals/ConfigDialog/index.tsx
@@ -1,12 +1,16 @@
-import { Box, Button, Dialog, IconButton, Tab, Tabs, TextField, Typography } from "@mui/material"
+import { Button, Dialog, IconButton, Tab, Tabs, Typography } from "@mui/material"
 
 import RefreshIcon from '@mui/icons-material/Refresh';
 import CloseIcon from '@mui/icons-material/Close';
-import { useGetConfigQuery, usePutConfigMutation, useReloadConfigMutation } from "@/api/v1/config/api";
+import { usePutConfigMutation, useReloadConfigMutation } from "@/api/v1/config/api";
 import { useState } from "react";
-import type { Config } from "@/api/v1/config/types";
 import { ConfigDialogActions, ConfigDialogContent, ConfigDialogTitle, StyledTabContent } from "./index.styles";
-import { AllConfigValuesTab } from "./LegacyTab";
+import { AllConfigValuesTab } from "./AllConfigValuesTab";
+import { useAppSelector } from "@/api";
+import { RippingPathsTab } from "./RippingPathsTab";
+import { ServerSettingsTab } from "./ServerSettingsTab";
+import { TmdbTab } from "./TmdbTab";
+import { MakeMkvTab } from "./MakeMkvTab";
 
 type Props = {
   open: boolean
@@ -35,6 +39,7 @@ const TabContent = (props: TabContentProps) => {
       hidden={value !== index}
       id={`vertical-tabpanel-${index}`}
       aria-labelledby={`vertical-tab-${index}`}
+      style={{width: "100%"}}
       {...other}
     >
       { value === index && (
@@ -49,39 +54,19 @@ const TabContent = (props: TabContentProps) => {
 export const ConfigDialog = ({ open, onClose = () => {} }: Props) => {
   const [putConfig, _putConfigResult] = usePutConfigMutation()
   const [reloadConfig, _reloadConfigResult] = useReloadConfigMutation()
-
   const [currentTab, setCurrentTab] = useState(0)
-  const [formData, setFormData] = useState<Partial<Config>>({})
 
-  const { data, isLoading, isSuccess } = useGetConfigQuery()
+  const config = useAppSelector(state => state.config)
 
-  formData.config_file = formData?.config_file ?? data?.config_file ?? '';
-  formData.source = formData?.source ?? data?.source ?? '';
-  formData.destination = formData?.destination ?? data?.destination ?? '';
-  formData.tmdb_token = formData?.tmdb_token ?? data?.tmdb_token ?? '';
-  formData.makemkvcon_path = formData?.makemkvcon_path ?? data?.makemkvcon_path ?? '';
-  // const log_level = formData?.log_level ?? data?.log_level ?? '';
-  formData.log_file = formData?.log_file ?? data?.log_file ?? '';
-  formData.temp_prefix = formData?.temp_prefix ?? data?.temp_prefix ?? '';
-  formData.frontend = formData?.frontend ?? data?.frontend ?? '';
-  formData.listen_port = formData?.listen_port ?? data?.listen_port ?? '';
-
-  // useEffect(() => {
-  //   if (!isLoading && isSuccess) {
-  //     setFormFromApi(data)
-  //   }
-  // }, [isLoading, isSuccess])
-  
   const handleClose = () => { onClose() }  
   
   const handleSave = () => { 
-    putConfig(formData)
+    putConfig(config)
     onClose() 
   }
 
   const handleRefresh = () => {
     reloadConfig()
-    setFormData({})
   }
 
   const handleChangeTab = (_event: React.SyntheticEvent, newValue: number) => {
@@ -102,17 +87,26 @@ export const ConfigDialog = ({ open, onClose = () => {} }: Props) => {
         onChange={handleChangeTab}
         sx={{ borderRight: 1, borderColor: 'divider'}}
       >
-        <Tab label="File Paths" {...a11yProps(0)} />
+        <Tab label="Ripping Paths" {...a11yProps(0)} />
         <Tab label="Server Settings" {...a11yProps(1)} />
         <Tab label="TMDB" {...a11yProps(2)} />
         <Tab label="MakeMKV" {...a11yProps(3)} />
         <Tab label="All Config Values" {...a11yProps(4)} />
       </Tabs>
       <TabContent index={0} value={currentTab}>
-        <Typography>Tab Two!</Typography>
+        <RippingPathsTab />
+      </TabContent>
+      <TabContent index={1} value={currentTab}>
+        <ServerSettingsTab />
+      </TabContent>
+      <TabContent index={2} value={currentTab}>
+        <TmdbTab />
+      </TabContent>
+      <TabContent index={3} value={currentTab}>
+        <MakeMkvTab />
       </TabContent>
       <TabContent index={4} value={currentTab}>
-        <AllConfigValuesTab formData={formData} setFormData={setFormData} />
+        <AllConfigValuesTab />
       </TabContent>
     </ConfigDialogContent>
     <ConfigDialogActions>
